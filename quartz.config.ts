3196d7e9fdfecce19479eb869d59f3c28b0d37d9
Replit user <>
2025-12-04 09:37:47 +0000

    meta

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4.0",
+    pageTitle: "absurd",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "absurd.ws",
+    baseUrl: "absurd.sh",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
